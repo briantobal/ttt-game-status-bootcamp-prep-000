c8eb4c8b49b8f1b027135a0da32cada0b6a40559
Brian Tobal <brian.tobal@gmail.com>
2018-07-15 15:02:41 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -135,7 +135,7 @@ end
 def winner(board)
   #binding.pry if board == ["X", " ", " ", " ", "X", " ", " ", " ", "X"]
   if won?(board) && won?(board).any?{|i| board[i] == "X"}
-    board[won?(board).first] == "X"
+    # board[won?(board).first] == "X"
     return "X"
   elsif won?(board) && won?(board).any?{|i| board[i] == "O"}
     return "O"
@@ -144,7 +144,7 @@ end
 
 
 WIN_COMBINATIONS = [
-    [0,1,2], # Top row
+   # [0,1,2], # Top row
     [3,4,5], # Middle row
     [6,7,8], # bottom row 
     [0,4,8], # top left diagonal 
