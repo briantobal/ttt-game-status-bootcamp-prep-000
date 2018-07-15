2ec354da04b8e47b6b0d567a79571fd7f76482f1
Brian Tobal <brian.tobal@gmail.com>
2018-07-15 13:28:17 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -74,7 +74,7 @@ end
 
 
 def over?(board)
-  if won?(board) == true && full?(board) == false
+  if full?(board) == false && won?(board) == true
     return true
   elsif full?(board) == true || draw?(board) == true
     return true 
