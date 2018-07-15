7a89c36dfd9ba2f4a3a44a2d0ce577931bf6f6f1
Brian Tobal <brian.tobal@gmail.com>
2018-07-15 12:43:13 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,6 +1,33 @@
-# Helper Method
+l# Helper Method
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
 # Define your WIN_COMBINATIONS constant
+
+
+def won?(board)
+  win_index = Array.new
+  unless board.any?
+    return false 
+  else board[0] == "X" and board[1] == "X" and board[2] == "X"
+win_index << 0
+win_index << 1
+win_index << 2    
+return win_index
+end
+end
+end
+WIN_COMBINATIONS = [
+    [0,1,2], # Top row
+    [3,4,5], # Middle row
+    [6,7,8], # bottom row 
+    [0,4,8], # top left diagonal 
+    [6,4,2], # bottom left diagonal 
+    [0,3,6], # left column 
+    [1,4,7], # middle column 
+    [2,5,8]  # bottom column 
+  ]
+  
+  
+# learn --fail-fast
